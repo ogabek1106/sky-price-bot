60986cd4cc4b358b6d9d47d62bd9fa05fe58f90c
ogabek1106 <84424884+ogabek1106@users.noreply.github.com>
2025-08-06 17:50:41 +0300

    Create amadeus_api.py

---
+++ amadeus_api.py
@@ -0,0 +1,33 @@
+import requests
+from config import AMADEUS_CLIENT_ID, AMADEUS_CLIENT_SECRET
+
+def get_access_token():
+    url = 'https://test.api.amadeus.com/v1/security/oauth2/token'
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    data = {
+        'grant_type': 'client_credentials',
+        'client_id': AMADEUS_CLIENT_ID,
+        'client_secret': AMADEUS_CLIENT_SECRET
+    }
+    response = requests.post(url, headers=headers, data=data)
+    response.raise_for_status()
+    return response.json()['access_token']
+
+
+def search_flights(origin, destination, date):
+    token = get_access_token()
+    url = 'https://test.api.amadeus.com/v2/shopping/flight-offers'
+    params = {
+        'originLocationCode': origin,
+        'destinationLocationCode': destination,
+        'departureDate': date,
+        'adults': 1,
+        'currencyCode': 'USD',
+        'max': 3
+    }
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    return response.json()
